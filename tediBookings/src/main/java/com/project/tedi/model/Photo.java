220ab8e04043c55d8cb8bfe3c0a15b420cf1347c
kkout <koutsompinaskk@gmail.com>
2023-09-06 15:24:42 +0300

    small change

--- tediBookings/src/main/java/com/project/tedi/model/Photo.java
+++ tediBookings/src/main/java/com/project/tedi/model/Photo.java
@@ -25,7 +25,7 @@ import lombok.NoArgsConstructor;
 public class Photo {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@NonNull
